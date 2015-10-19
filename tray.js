4697ffc23aad550e8cc624872aaf0b9b3188cd56
not-much-io <kristo.koert@gmail.com>
2015-10-19 23:20:12 +0300

    Made into desktop app, with tray icon.

---
+++ tray.js
@@ -0,0 +1,10 @@
+
+(function() {
+
+    // Load native UI library
+    var gui = require('nw.gui');
+
+    // Create a tray icon
+    var tray = new gui.Tray({ title: 'Tray', icon: 'resources/abstract.png' });
+
+})();
