ddad36145dc1b10eb0c471c2e714e6f103dca15c
not-much-io <kristo.koert@gmail.com>
2015-10-20 22:26:16 +0300

    Added minimize to tray functionality.

--- tray.js
+++ tray.js
@@ -4,7 +4,27 @@
     // Load native UI library
     var gui = require('nw.gui');
 
-    // Create a tray icon
-    var tray = new gui.Tray({ title: 'Tray', icon: 'resources/abstract.png' });
+    // Reference to window
+    var win = gui.Window.get();
 
+    // Declare tray
+    var tray;
+
+    // Get the minimize event
+    win.on('minimize', function() {
+
+        // Hide window
+        this.hide();
+
+        // Show tray
+        tray = new gui.Tray({ title: 'Tray', icon: 'resources/abstract.png' });
+
+        // Show window and remove tray when clicked
+        tray.on('click', function() {
+            win.show();
+            this.remove();
+            tray = null;
+        });
+
+    });
 })();
